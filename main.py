ee4da718e13160b6f56fbccdc00b5ff1d3aad99e
MarcAntoineAlex <18510756258@163.com>
2021-11-21 11:23:26 +0800

    full loading

--- main.py
+++ main.py
@@ -119,8 +119,8 @@ def worker(gpu, ngpus_per_node, args_in):
         if args.do_predict:
             logger.info('>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
             exp.predict(setting, True)
-        os.remove(args.path + '{}/0_checkpoint.pth'.format(ii))
-        os.remove(args.path + '{}/1_checkpoint.pth'.format(ii))
+        os.remove(args.path + '/{}/0_checkpoint.pth'.format(ii))
+        os.remove(args.path + '/{}/1_checkpoint.pth'.format(ii))
 
         torch.cuda.empty_cache()
     logger.info("R{} FINAL RESULT {} {}".format(args.rank, torch.tensor(mses).mean(), torch.tensor(maes).mean()))
