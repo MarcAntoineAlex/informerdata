0420b15db375d8a1c7d078f93e5feeb1cbcb1c42
MarcAntoineAlex <18510756258@163.com>
2021-12-09 20:58:53 +0800

    back to normal

--- exp/exp_m.py
+++ exp/exp_m.py
@@ -195,6 +195,7 @@ class Exp_M_Informer(Exp_Basic):
                     else:
                         print("data_count ", data_count, self.model.arch.grad[:100].squeeze(), self.model.arch[:100].squeeze())
                 A_optim.step()
+                print(self.model.arch[:100].squeeze())
                 W_optim.zero_grad()
                 pred = torch.zeros(trn_data[1][:, -self.args.pred_len:, :].shape).to(self.device)
                 if self.args.rank == 0:
@@ -214,6 +215,7 @@ class Exp_M_Informer(Exp_Basic):
                         loss.backward()
                         W_optim.step()
                 train_loss.append(loss.item())
+                print(self.model.arch[:100].squeeze())
 
                 if (i + 1) % 50 == 0:
                     logger.info("\tR{0} iters: {1}, epoch: {2} | loss: {3:.7f}".format(self.args.rank, i + 1, epoch + 1, loss.item()))
