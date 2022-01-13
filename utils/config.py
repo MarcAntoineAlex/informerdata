a02c21ec22ead84e6cbb9b66a6bc4332d1d3c476
MarcAntoineAlex <18510756258@163.com>
2022-01-14 00:04:21 +0800

    back to normal

--- utils/config.py
+++ utils/config.py
@@ -52,7 +52,7 @@ class MInformerConfig(BaseConfig):
         parser.add_argument('--model', type=str, default='informer',
                             help='model of experiment, options: [informer, informerstack, informerlight(TBD)]')
         parser.add_argument('--data', type=str, default='ETTh1', help='data')
-        parser.add_argument('--root_path', type=str, default='/home/LAB/gaoch/asdf/data/ETDataset/ETT-small/', help='root path of the data file')
+        parser.add_argument('--root_path', type=str, default='/home/LAB/gaoch/data/ETDataset/ETT-small/', help='root path of the data file')
         parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
         parser.add_argument('--features', type=str, default='M',
                             help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
