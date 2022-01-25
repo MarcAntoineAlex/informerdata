d3b30291eb0bb2b6d65e414516c50f5588a3549d
MarcAntoineAlex <18510756258@163.com>
2022-01-25 17:41:49 +0800

    back to normal

--- main.py
+++ main.py
@@ -131,6 +131,7 @@ def worker(gpu, ngpus_per_node, args_in):
         if args.do_predict:
             logger.info('>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
             exp.predict(setting, ii, True)
+        dist.barrier()
         if args.rank == 0:
             os.remove(args.path + '/{}/0_checkpoint.pth'.format(ii))
             os.remove(args.path + '/{}/1_checkpoint.pth'.format(ii))
